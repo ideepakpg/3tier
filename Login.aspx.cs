0c5d1466874a9c5b2f029e4e5deb43370a861682
Deepak PG <deepakpg121@gmail.com>
2023-09-16 15:56:58 +0530

    Implement Login Page Code-Behind

---
+++ Login.aspx.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogicLayer;
+using System;
+using System.Data.SqlClient;
+
+namespace _3tier_regform
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        BLL obj = new BLL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["username"] = TextBox1.Text;
+            Session["password"] = TextBox2.Text;
+
+            SqlDataReader rdr = obj.sid(TextBox1.Text, TextBox2.Text);
+            if (rdr.Read())
+            {
+                Response.Redirect("LoginWelcome.aspx");
+            }
+        }
+    }
+}
